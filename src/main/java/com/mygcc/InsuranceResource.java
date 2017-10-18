98f99c4ba16ce229373ad36a1d8e76fc41127eb7
mph121 <harveymp1@gcc.edu>
2017-10-18 16:00:14 -0400

    Build succeeds now and added unit tests

--- src/main/java/com/mygcc/InsuranceResource.java
+++ src/main/java/com/mygcc/InsuranceResource.java
@@ -1,22 +1,77 @@
 package com.mygcc;
 
-import javax.ws.rs.*;
+import javax.ws.rs.GET;
+import javax.ws.rs.HeaderParam;
+import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
-import java.net.HttpURLConnection;
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.net.URL;
+import java.net.URLConnection;
 import java.util.HashMap;
 import java.util.Map;
 
-
+/**
+ * Class for getting insurance information.
+ */
 @Path("/1/user/")
 public class InsuranceResource {
+
+    /**
+     * Main function for getting insurance.
+     * @param token Student id.
+     * @return formatted insurance information.
+     * @throws Exception When url is not formatted right.
+     */
     @GET
     @Path("insurance")
     @Produces(MediaType.APPLICATION_JSON)
-    public Response insuranceResponse(@HeaderParam("Authorization") String token) {
-        HttpURLConnection.setFollowRedirects(true);
+    public final Response insuranceResponse(
+            @HeaderParam("Authorization") final String token)
+            throws Exception {
+        // Check if token is null.
+        if (token == null) {
+            return Response.status(Response.Status.BAD_REQUEST)
+                    .entity("No token in authorization header.")
+                    .build();
+        }
+        // Else get insurance info.
         Map<String, Object> response = new HashMap<>();
-        response.put("token", token);
-        return Response.status();
+        String insuranceInfo = getContentFromUrl(token);
+
+        //Check if insurance info exists.
+        if (insuranceInfo.equals("null")) {
+            return Response.status(Response.Status.NOT_FOUND)
+                    .entity("Insurance info not found.")
+                    .build();
+        }
+        response.put("insurance", insuranceInfo);
+        return Response.status(Response.Status.OK)
+                .entity(response)
+                .build();
+    }
+
+    /**
+     * Get insurance info for given id.
+     * @param id student id number.
+     * @return JSON string from myGCC
+     * @throws Exception When url is not formatted right.
+     */
+    private String getContentFromUrl(final String id) throws Exception {
+        String gccUrl = "https://my.gcc.edu/html5/apps/fin/models/JSON.ashx";
+        String insParams = "?entity=healthinsurance&qry=get&id_num=";
+        URL gccIns = new URL(gccUrl + insParams + id);
+        URLConnection yc = gccIns.openConnection();
+        BufferedReader in = new BufferedReader(new InputStreamReader(
+                yc.getInputStream()));
+        String inputLine;
+        StringBuilder output = new StringBuilder();
+        while ((inputLine = in.readLine()) != null) {
+            output.append(inputLine);
+        }
+        in.close();
+        return output.toString();
     }
 }
---
+++ src/test/java/com/mygcc/InsuranceResourceTest.java
@@ -0,0 +1,67 @@
+package com.mygcc;
+
+import org.glassfish.jersey.server.ResourceConfig;
+import org.glassfish.jersey.test.JerseyTest;
+import org.glassfish.jersey.test.TestProperties;
+import org.json.JSONObject;
+import org.junit.Test;
+
+import javax.ws.rs.core.Application;
+import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
+import java.time.Instant;
+import java.util.Map;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.assertTrue;
+
+/**
+ * Welcome Resource test class.
+ */
+public final class InsuranceResourceTest extends JerseyTest {
+
+    @Override
+    protected Application configure() {
+        enable(TestProperties.LOG_TRAFFIC);
+        enable(TestProperties.DUMP_ENTITY);
+        return new ResourceConfig(InsuranceResource.class);
+    }
+
+    /**
+     * Test Insurance response status with valid Id.
+     */
+    @Test
+    public void testValidInsuranceId() {
+        final Response response = target().path("/1/user/insurance").request().header("Authorization", 207783).get();
+        int status = response.getStatus();
+        assertEquals("should return status code 200",
+                Response.Status.OK.getStatusCode(),
+                status);
+        assertNotNull("should not return null", response.getEntity());
+    }
+
+    /**
+     * Test Insurance response status with missing Id.
+     */
+    @Test
+    public void testMissingInsuranceId() {
+        final Response response = target().path("/1/user/insurance").request().get();
+        int status = response.getStatus();
+        assertEquals("should return status code 400",
+                Response.Status.BAD_REQUEST.getStatusCode(),
+                status);
+    }
+
+    /**
+     * Test Insurance response status with invalid Id.
+     */
+    @Test
+    public void testInvalidInsuranceId() {
+        final Response response = target().path("/1/user/insurance").request().header("Authorization", "666").get();
+        int status = response.getStatus();
+        assertEquals("should return status code 404",
+                Response.Status.NOT_FOUND.getStatusCode(),
+                status);
+    }
+}
