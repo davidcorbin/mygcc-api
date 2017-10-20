e9e8837142da03d0eb2c80471ac3101a3adfe0d4
David Corbin <daconex@gmail.com>
2017-10-20 15:38:34 -0400

    Updated package info

--- src/main/java/com/mygcc/api/package-info.java
+++ src/main/java/com/mygcc/api/package-info.java
@@ -1,4 +1,12 @@
 /**
- * Package-info for api package.
+ * API endpoint implementation.
+ *
+ * The API package contains the implementation behind the HTTP API endpoints.
+ * This should be where the network requests are received and responses are
+ * created.
+ *
+ * @author David Corbin (@davidcorbin)
+ * @author Mitchell Harvey (@mph121)
+ * @since 1.0
  */
 package com.mygcc.api;
