ae8d1f07e0604841079ad7dfe54a90289a352b0c
David Corbin <daconex@gmail.com>
2017-10-20 13:28:36 -0400

    Added error handling package info

--- src/main/java/com/mygcc/errorhandling/package-info.java
+++ src/main/java/com/mygcc/errorhandling/package-info.java
@@ -1,4 +1,11 @@
 /**
- * Package-info for error handling package.
+ * API error handling.
+ *
+ * The error handling package deals with errors resulting from client error and
+ * internal error.
+ *
+ * @author David Corbin (@davidcorbin)
+ * @author Mitchell Harvey (@mph121)
+ * @since 1.0
  */
 package com.mygcc.errorhandling;
