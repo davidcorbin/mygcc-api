4ad60f12319a1a209cc1432fd150e0ebf1c4fb39
David Corbin <daconex@gmail.com>
2017-10-20 15:39:53 -0400

    Updated package info for mygcc dir

--- src/main/java/com/mygcc/package-info.java
+++ src/main/java/com/mygcc/package-info.java
@@ -1,4 +1,11 @@
 /**
- * Package-info for mygcc package.
+ * myGCC package.
+ *
+ * The myGCC package contains any logic that doesn't belong in another
+ * package but is necessary as part of the project.
+ *
+ * @author David Corbin (@davidcorbin)
+ * @author Mitchell Harvey (@mph121)
+ * @since 1.0
  */
 package com.mygcc;
