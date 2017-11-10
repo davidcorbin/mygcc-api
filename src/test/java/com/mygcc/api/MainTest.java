bef8cc96a24a9b6b142a65b6a9d2c10e2d326239
David Corbin <daconex@gmail.com>
2017-11-09 20:52:46 -0500

    Added server start test

---
+++ src/test/java/com/mygcc/api/MainTest.java
@@ -0,0 +1,54 @@
+package com.mygcc.api;
+
+import com.mygcc.datacollection.Authorization;
+import org.glassfish.jersey.server.ResourceConfig;
+import org.glassfish.jersey.test.JerseyTest;
+import org.glassfish.jersey.test.TestProperties;
+import org.junit.Test;
+import org.eclipse.jetty.http.HttpStatus;
+import org.eclipse.jetty.server.Server;
+import org.eclipse.jetty.servlet.DefaultServlet;
+import org.eclipse.jetty.servlet.ServletContextHandler;
+import org.eclipse.jetty.servlet.ServletHolder;
+
+import javax.ws.rs.core.Application;
+import java.net.HttpURLConnection;
+import java.net.URL;
+
+import static junit.framework.TestCase.assertEquals;
+
+public class MainTest extends JerseyTest {
+    @Override
+    protected Application configure() {
+        enable(TestProperties.LOG_TRAFFIC);
+        enable(TestProperties.DUMP_ENTITY);
+        return new ResourceConfig(Authorization.class);
+    }
+
+    /**
+     * Test that server starts normally
+     * @throws Exception generic exception throw when something goes wrong
+     */
+    @Test
+    public void testDefaultConstructor() throws Exception {
+        // Create Server
+        Server server = new Server(8080);
+        ServletContextHandler context = new ServletContextHandler();
+        ServletHolder defaultServ = new ServletHolder("default", DefaultServlet.class);
+        defaultServ.setInitParameter("resourceBase", System.getProperty("user.dir"));
+        defaultServ.setInitParameter("dirAllowed","true");
+        context.addServlet(defaultServ,"/");
+        server.setHandler(context);
+
+        // Start Server
+        server.start();
+
+        // Test GET
+        HttpURLConnection http = (HttpURLConnection)new URL("http://localhost:8080/").openConnection();
+        http.connect();
+        assertEquals("Response Code", http.getResponseCode(), HttpStatus.OK_200);
+
+        // Stop Server
+        server.stop();
+    }
+}
