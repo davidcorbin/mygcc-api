935b7734e428602b9b188ca65a0105bee7110952
mph121 <HarveyMP1@gcc.edu>
2017-11-13 10:46:10 -0500

    Fixed checkstyle violations

--- src/main/java/com/mygcc/api/ScheduleResource.java
+++ src/main/java/com/mygcc/api/ScheduleResource.java
@@ -11,9 +11,7 @@ import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
-import java.util.HashMap;
 import java.util.List;
-import java.util.Map;
 
 /**
  * Schedule resource endpoint.
