5a0a96e32b4c0f274ece85d2d90a6cbefef9de3c
David Corbin <daconex@gmail.com>
2017-11-24 16:52:07 -0500

    Added Token tests

---
+++ src/test/java/com/mygcc/datacollection/TokenTest.java
@@ -0,0 +1,23 @@
+package com.mygcc.datacollection;
+
+import org.glassfish.jersey.server.ResourceConfig;
+import org.glassfish.jersey.test.JerseyTest;
+import org.glassfish.jersey.test.TestProperties;
+import org.junit.Test;
+
+import javax.ws.rs.core.Application;
+
+public class TokenTest extends JerseyTest {
+    @Override
+    protected Application configure() {
+        enable(TestProperties.LOG_TRAFFIC);
+        enable(TestProperties.DUMP_ENTITY);
+        return new ResourceConfig(Token.class);
+    }
+
+    @Test(expected = InvalidCredentialsException.class)
+    public void testNullToken() throws InvalidCredentialsException {
+        new Token(null);
+    }
+
+}
