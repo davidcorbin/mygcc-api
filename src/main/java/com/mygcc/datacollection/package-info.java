fb5b25d3ca6218a9bd64891cc8d3c860b8be797d
David Corbin <daconex@gmail.com>
2017-12-05 16:22:17 -0500

    Rewrote package info

--- src/main/java/com/mygcc/datacollection/package-info.java
+++ src/main/java/com/mygcc/datacollection/package-info.java
@@ -1,8 +1,8 @@
 /**
  * myGCC data collection.
  *
- * The data collection package contains logic with myGCC. THe package should
- * send requests to my.gcc.edu and other relevant URLs to retrieve pertinent
+ * The data collection package 'talks' to myGCC. The package should send
+ * requests to my.gcc.edu and other relevant URLs to retrieve pertinent
  * information.
  *
  * @author David Corbin (@davidcorbin)
